416d1717bc9c972d005e38cde1777c23d9e998b3
Nazhir Jackson <naz@codedifferently.com>
2019-09-17 15:36:17 -0400

    added core module and more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+  path: '',
+  component: ContentLayoutComponent,
+  children: [
+    {
+      path: 'home',
+      loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+    }
+  ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component'
+import { NavComponent } from './layout/nav/nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    ContentLayoutComponent
   ],
   imports: [
     BrowserModule,
